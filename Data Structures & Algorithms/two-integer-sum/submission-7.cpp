f8df34e60242969a788a52123616dc54230dc59f
Aortz <65661817+Aortz@users.noreply.github.com>
2026-05-22 22:59:27 +0800

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; // value -> index
+        for (int i = 0; i < static_cast<int>(nums.size()); ++i){
+            int need = target - nums[i];
+
+            if (seen.count(need)){
+                return {seen[need], i};
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
